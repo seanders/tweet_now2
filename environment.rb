ef591ac79612f417f4d698f595f31ec9654be296
Apprentice <apprentice@devbootcamp.com>
2013-05-07 12:45:46 -0700

    reinitializes this repo

---
+++ environment.rb
@@ -0,0 +1,47 @@
+# Set up gems listed in the Gemfile.
+# See: http://gembundler.com/bundler_setup.html
+#      http://stackoverflow.com/questions/7243486/why-do-you-need-require-bundler-setup
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# Require gems we care about
+require 'rubygems'
+
+require 'uri'
+require 'pathname'
+
+require 'pg'
+require 'active_record'
+require 'logger'
+
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+require 'erb'
+require 'twitter'
+
+# Some helper constants for path-centric logic
+APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
+
+APP_NAME = APP_ROOT.basename.to_s
+
+# Set up the controllers and helpers
+Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
+Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
+
+# Set up the database and models
+require APP_ROOT.join('config', 'database')
+
+
+if development?
+  twitter_config = YAML.load(File.read(APP_ROOT.join("config", "application.yml")))
+  twitter_config.each {|k, v| ENV[k] = v}
+
+  Twitter.configure do |config|
+    config.consumer_key = ENV['CONSUMER_KEY']
+    config.consumer_secret = ENV["CONSUMER_SECRET"]
+    config.oauth_token = ENV["OAUTH_TOKEN"]
+    config.oauth_token_secret = ENV["OAUTH_TOKEN_SECRET"]
+  end
+end
